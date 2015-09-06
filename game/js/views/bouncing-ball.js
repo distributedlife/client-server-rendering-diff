55c2d7e84d1571f19ed0887ded2c522d9913e8e1
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-09-07 08:11:51 +1000

    migrate demo to use 5.0.0-beta-rc1 of ensemble

--- game/js/views/bouncing-ball.js
+++ game/js/views/bouncing-ball.js
@@ -1,69 +1,67 @@
 'use strict';
 
-var $ = require('zepto-browserify').$;
+function ballColour (demeanour) {
+  if (demeanour === 'happy') {
+    return '#ffffff';
+  } else {
+    return '#ff0000';
+  }
+}
+
+function drawBall (context, position, demeanour, radius) {
+  context.fillStyle = ballColour(demeanour);
+  context.beginPath();
+  context.arc(position.x, position.y, radius, 0, 2*Math.PI);
+  context.closePath();
+  context.fill();
+}
+
+function drawBoard (context, dimensions) {
+  context.fillStyle = '#55ff55';
+  context.fillRect(0, 0, dimensions.width, dimensions.height);
+}
+
+function theBallPosition (state) {
+  return state['bouncing-ball-game'].ball.position;
+}
+
+function theBallRadius (state) {
+  return state['bouncing-ball-game'].ball.radius;
+}
+
+function theBallDemeanour (state) {
+  return state['bouncing-ball-game'].ball.demeanour;
+}
+
+function theBoardDimensions (state) {
+  return state['bouncing-ball-game'].board;
+}
+
+function calculateOffset (boardDimensions, screenDimensions) {
+  return {
+    x: (screenDimensions.usableWidth - boardDimensions.width) / 2,
+    y: (screenDimensions.usableHeight - boardDimensions.height) / 2
+  };
+}
 
 module.exports = {
-  type: 'View',
-  deps: ['Element', 'DefinePlugin', 'CurrentState'],
-  func: function (element, define, currentState) {
-    var ballColour = function(demeanour) {
-      if (demeanour === 'happy') {
-        return '#ffffff';
-      } else {
-        return '#ff0000';
-      }
-    };
-
-    var drawBall = function(context, position, demeanour, radius) {
-      context.fillStyle = ballColour(demeanour);
-      context.beginPath();
-      context.arc(position.x, position.y, radius, 0, 2*Math.PI);
-      context.closePath();
-      context.fill();
-    };
-
-    var drawBoard = function(context, dimensions) {
-      context.fillStyle = '#55ff55';
-      context.fillRect(0, 0, dimensions.width, dimensions.height);
-    };
-
-    var theBallPosition = function (state) {
-      return state['bouncing-ball-game'].ball.position;
-    };
-
-    var theBallRadius = function (state) {
-      return state['bouncing-ball-game'].ball.radius;
-    };
-
-    var theBallDemeanour = function (state) {
-      return state['bouncing-ball-game'].ball.demeanour;
-    };
-
-    var theBoardDimensions = function (state) {
-      return state['bouncing-ball-game'].board;
-    };
-
-    var calculateOffset = function (boardDimensions, screenDimensions) {
-      return {
-        x: (screenDimensions.usableWidth - boardDimensions.width) / 2,
-        y: (screenDimensions.usableHeight - boardDimensions.height) / 2
-      };
-    };
-
+  type: 'OnReady',
+  deps: ['Config', 'DefinePlugin', 'CurrentState', '$'],
+  func: function View (config, define, currentState, $) {
     var offset;
-    return function (dims) {
-      var canvas = $('<canvas/>', { id: 'scene' });
+    return function setup (dims) {
+      var canvas = $()('<canvas/>', { id: 'scene' });
       canvas[0].width = dims.usableWidth;
       canvas[0].height = dims.usableHeight;
       var context = canvas[0].getContext('2d');
 
-      $('#' + element()).append(canvas);
+      $()('#' + config().client.element).append(canvas);
 
       offset = calculateOffset(currentState().get(theBoardDimensions), dims);
       context.translate(offset.x, offset.y);
 
-      define()('OnEachFrame', function () {
-        return function () {
+      define()('OnRenderFrame', function () {
+        return function renderScene () {
           context.clearRect(0, 0, canvas[0].width, canvas[0].height);
           drawBoard(context, currentState().get(theBoardDimensions));
           drawBall(context, currentState().get(theBallPosition), currentState().get(theBallDemeanour), currentState().get(theBallRadius));
@@ -71,7 +69,7 @@ module.exports = {
       });
 
       define()('OnResize', function () {
-        return function (dims) {
+        return function resizeScene (dims) {
           canvas[0].width = dims.usableWidth;
           canvas[0].height = dims.usableHeight;
           offset = calculateOffset(currentState().get(theBoardDimensions), dims);
